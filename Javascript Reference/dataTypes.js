ba82f1b2f243c440c38ab7fc8b1afb15ccfe7675
08042004Aditysingh <118094804+08042004Aditysingh@users.noreply.github.com>
2023-06-10 12:14:35 +0530

    first commit

---
+++ Javascript Reference/dataTypes.js
@@ -0,0 +1,11 @@
+console.log("hello, World!");
+
+/*Data Types are of two types in broader horizon
+1)Primitive data type:
+a)Strings
+b)Numbers
+c)Booleans
+d)Null
+e)Undefined
+2)Complex data type
+*/
---
+++ Javascript Reference/variable.js
@@ -0,0 +1,38 @@
+console.log("hello world!");
+console.log("Test");
+console.log("Welcome to Variables");
+/* Variables are the container that store data
+
+e.g: A BOOK in the BOX
+
+here BOX is the "Variable" and BOOK is the "Data"*/
+
+var variableName1 = 'Welcome to variables';
+
+console.log(variableName1); 
+
+/*===========OR=============*/
+
+let variableName2 = 'Welcome to variables';
+
+console.log(variableName2);
+
+/*'let' variable keyword has some scope constraints but preffered
+Variable name can be reassign but can not be redeclared*/
+
+/*==========OR=============*/
+
+const variableName3 = 'Welcome to variables';
+
+console.log(variableName3);
+
+/*Variable name can nither be reassign or redeclared */
+
+/* Rules for creating Identifiers (Variable name) 
+1)It must be unique i.e. two variable's name can't be same
+2)Name of identifier should not be any reserved javasript keyword
+3)First character must be a 'letter' or 'underscore' or '$'
+4)Rest of the character can be 'digit', 'letter', 'underscore', '$' */
+
+
+
