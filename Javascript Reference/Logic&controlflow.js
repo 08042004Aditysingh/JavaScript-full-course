534cf6d68f933c1aed33bed0945d8760bc3e4daa
08042004Aditysingh <118094804+08042004Aditysingh@users.noreply.github.com>
2023-06-10 22:27:20 +0530

    sixth commit

--- Javascript Reference/Logic&controlflow.js
+++ Javascript Reference/Logic&controlflow.js
@@ -7,22 +7,22 @@
 
 //If Statements
 
-const age2=18;
+const age2 = 18;
 
-if(age2 > 18){
+if (age2 > 18) {
     console.log('You may enter!');
-}else if(age2===18){
+} else if (age2 === 18) {
     console.log('You just turned 18, welcome!');
 }
-else{
+else {
     console.log('Grow Up!');
 }
 
 //The "While" loop
 //A single execution of a loop body is called iteration
-let i=0;
+let i = 0;
 
-while(i<3){
+while (i < 3) {
     console.log(i);
     i++;
 }
@@ -31,8 +31,8 @@ while(i<3){
 
 //for([initialisation];[condition];[final-expression]){ //block of code };
 
-for(let i=0;i<3;i++){
-     console.log(i);
+for (let i = 0; i < 3; i++) {
+    console.log(i);
 }
 
 
@@ -51,3 +51,39 @@ console.log(4);
 console.log(5);
 console.log(6);
 */
+
+//Ternary Operator
+
+// {[condition]?[execution for true]:[execution for false]};
+
+{ 2 > 1 ? console.log('Hello!') : console.log('Hola!') };
+
+//Switch Statements
+
+//getDay() method returns the weekdays as number between 0 to 6
+
+switch (new Date().getDay()) {
+    case 0:
+        day = "sunday";
+        break;
+    case 1:
+        day = "monday";
+        break;
+    case 2:
+        day = "tuesday";
+        break;
+    case 3:
+        day = "wednesday";
+        break;
+    case 4:
+        day = "thursday";
+        break;
+    case 5:
+        day = "friday";
+        break;
+    case 6:
+        day = "saturday";
+        break;
+}
+console.log(day);
+
